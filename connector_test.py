588edf2f3d357b6d4fac42d7481d146ae68900b9
Elizabeth Onafuwa <elizabethonafuwa@gmail.com>
2024-04-28 18:57:33 -0400

    2nd Commit

---
+++ connector_test.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+ 
+
+connection = mysql.connector.connect (
+    user = 'root' ,
+    database = 'elite102' ,
+    password = 'hhhggg123' ,
+)
+
+cursor = connection.cursor()
+testQuery = ("SELECT * FROM user_accounts")
+testQuery = ("SELECT * FROM transactions")
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+connection.commit()
+
+cursor.close()
+connection.close()
